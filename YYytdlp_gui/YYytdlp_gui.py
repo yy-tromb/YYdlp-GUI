42ca20fc753e59aa032e21c24b2e74c137cc907a
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-08-15 17:46:02 +0900

    fix page change

--- YYytdlp_gui/YYytdlp_gui.py
+++ YYytdlp_gui/YYytdlp_gui.py
@@ -1,6 +1,5 @@
-import flet
 from view import View
 
 if __name__ == "__main__":
     view=View()
-    
+    view.run()
--- YYytdlp_gui/view.py
+++ YYytdlp_gui/view.py
@@ -1,21 +1,80 @@
-import flet
+import flet as ft
+import mycontrols as myctls
+
 
 def __init__():
     pass
 
+
 class View:
-    def __init__(self) -> "View":
-        flet.app(target=self.main)
-        self
-    
-    def main(self,page :flet.Page):
-        page.title="YYytdlp_gui v0.1"
-        page.vertical_alignment = flet.MainAxisAlignment.CENTER
-        page.horizontal_alignment = flet.MainAxisAlignment.CENTER
-        # page.add(flet.Text(value="hoge",text_align=flet.TextAlign.CENTER))
-        page.views.append(MainView().view)
-        page.update()
+    def __init__(self) -> None:
+        self.views = ["main", "setting"]
+
+    def run(self) -> None:
+        ft.app(target=self.main)
+
+
+    def main(self, page: ft.Page) -> None:
+        self.page = page
+        page.title = "YYytdlp_gui v0.1"
+        page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+        # page.add(ft.Text(value="hoge",text_align=ft.TextAlign.CENTER))
+        # ↑ code for not multiview (memo)
+        self.mainView = MainView(page)
+        self.settingsView = SettingsView(page)
+
+        page.on_route_change = self.__on_route_change
+        page.on_view_pop = self.__on_pop_view
+
+        page.views.clear()
+        page.go("/main")
+
+    def __on_route_change(self, handler):
+        # troupe = ft.TemplateRoute(handler.route)
+        self.page.views.clear()
+        self.page.views.append(self.mainView.view)
+        if self.page.route=="/settings":
+            self.page.views.append(self.settingsView.view)
+        # elif troupe.match("/main"):
+        #    self.page.views.append(MainView(self.page).view)
+        # "/main" has already appended
+        self.page.update()
+
+    def __on_pop_view(self, handler):
+        self.page.views.pop()
+        if len(self.page.views) > 1:
+            self.page.update()
+        else:
+            self.page.go("/main") # for all page not deleted
 
 class MainView:
-    def __init__(self) -> "MainView":
-        self.view=flet.View("/main",appbar=flet.AppBar(title="YYytdlp_gui v0.1",bgcolor=flet.colors.ORANGE),controls=[flet.Text(value="hoge",text_align=flet.TextAlign.CENTER)])
+    def __init__(self, page: ft.Page) -> None:
+        self.page=page # for page button
+        self.view = ft.View(
+            route="/main",
+            appbar=myctls.MyAppBar(page=page,title="YYytdlp_gui v0.1"),
+            controls=[
+                ft.Text(value="hoge", text_align=ft.TextAlign.CENTER),
+                ft.TextField()
+            ],
+        )
+
+
+class SettingsView:
+    def __init__(self, page: ft.Page) -> None:
+        self.page=page # for page button
+        self.view = ft.View(
+            route="/settings",
+             appbar=ft.AppBar(
+                title=ft.Text("YYytdlp_gui v0.1 Settings"),
+                bgcolor=ft.colors.ORANGE,
+            ),
+            controls=[
+                
+                ft.Text(
+                    value="Settings is now developping.You can't available now",
+                ),
+                ft.TextField()
+            ],
+        )
