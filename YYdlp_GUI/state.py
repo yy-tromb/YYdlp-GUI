a7415bbae30621f47a2f439497ff9eb885b307c4
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-08-18 00:28:01 +0900

    property hide

--- YYdlp_GUI/state.py
+++ YYdlp_GUI/state.py
@@ -4,17 +4,17 @@ T = TypeVar('T')
 
 class State(Generic[T]):
     def __init__(self, value: T):
-        self._value = value
-        self._observers: list[Callable] = []
+        self.__value = value
+        self.__observers: list[Callable] = []
 
     def get(self):
-        return self._value
+        return self.__value
 
     def set(self, new_value: T):
-        if self._value != new_value:
-            self._value = new_value
-            for observer in self._observers:
+        if self.__value != new_value:
+            self.__value = new_value
+            for observer in self.__observers:
                 observer()
 
     def bind(self, observer):
-        self._observers.append(observer)
+        self.__observers.append(observer)
