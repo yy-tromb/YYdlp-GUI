dff5a6726eaf26ac2e5cbb3758e3fe5a0db69342
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-08-23 13:33:32 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-pyinstaller ./YYdlp_GUI/YYdlp_GUI.spec -y
-for %%f in (./YYdlp_GUI/*.py) do copy "YYdlp_GUI\%%f" "YYdlp_GUI\dist\YYdlp_GUI\%%f"
+cd ./YYdlp_GUI
+pyinstaller ./YYdlp_GUI.spec -y
+for %%f in (./*.py) do copy "%%f" "dist\YYdlp_GUI\%%f"
