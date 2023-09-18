c7dd8ad9cc0607f28856e5f6ac373c60579da9c3
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-09-18 11:11:19 +0900

    update and add comment to state.State

--- YYdlp_GUI/state.py
+++ YYdlp_GUI/state.py
@@ -2,19 +2,32 @@ from typing import TypeVar, Generic, Union, Callable
 
 T = TypeVar('T')
 
-class State(Generic[T]):
-    def __init__(self, value: T):
-        self.__value = value
-        self.__observers: list[Callable] = []
 
-    def get(self):
-        return self.__value
+class State(Generic[T]): 
+"""
+When value is changed( setted by .set() ), functions( setted by .bind() ) is executed.  
+This is based in [this article: Python(Flet)でリアクティブなUIを作る方法を考える](https://qiita.com/ForestMountain1234/items/64edacd5275c1ce4c943). And I did a little changes.  
+Thanks to ForestMountain1234  
+[ForestMountain1234's GitHub](https://github.com/ForestMountain1234)  
+[ForestMountain1234's Qiita](https://qiita.com/ForestMountain1234/)
+"""
 
-    def set(self, new_value: T):
-        if self.__value != new_value:
-            self.__value = new_value
-            for observer in self.__observers:
-                observer()
+     def __init__(self, value: T,group: str | None=None): 
+         self.__value = value 
+         self.__observers: list[Callable[T]] = [] 
+         self.group = group
+  
+     def get(self): 
+         return self.__value 
+  
+     def set(self, new_value: T): 
+         if self.__value != new_value: 
+             self.__value = new_value 
+             self.update()
 
-    def bind(self, observer):
-        self.__observers.append(observer)
+    def update():
+      for observer in self.__observers: 
+                 observer(self.__value) 
+  
+     def bind(self, observer): 
+         self.__observers.append(observer)
