aea70db63ade751dc2d7a03589e23f21988c420d
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-09-21 01:23:57 +0900

    clean debug code

--- YYdlp_GUI/__main__.py
+++ YYdlp_GUI/__main__.py
@@ -1,13 +1,8 @@
 import sys
-
 import os.path
+
 path = os.path.realpath(os.path.abspath(__file__))
-print(5,__package__)
-print(6,__file__,os.path.abspath(__file__),path)
-import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
-print(9,os.path.dirname(os.path.dirname(path)))
-print(10,__name__)
 
 if __package__ is None and not getattr(sys, 'frozen', False):
     # direct call of __main__.py
--- YYdlp_GUI/state.py
+++ YYdlp_GUI/state.py
@@ -2,8 +2,6 @@ from typing import TypeVar, Generic, Callable, Union
 import abc
 
 T = TypeVar("T")
-T_STATE = TypeVar("T_STATE")
-T_REACTIVESTATE = TypeVar("T_REACTIVESTATE")
 
 
 class IState(Generic[T], metaclass=abc.ABCMeta):
@@ -20,23 +18,23 @@ class IState(Generic[T], metaclass=abc.ABCMeta):
         raise NotImplementedError()
 
 
-class State(Generic[T_STATE], IState):
+class State(IState,Generic[T]):
     """
     When value is changed( setted by .set() ), functions( setted by .bind() ) is executed.
 
     This is based in [this article: Python(Flet)でリアクティブなUIを作る方法を考える](https://qiita.com/ForestMountain1234/items/64edacd5275c1ce4c943). And I did a little changes.
     Thanks to ForestMountain1234
     [ForestMountain1234's GitHub](https://github.com/ForestMountain1234)
-    [ForestMountain1234's Qiita](https://qiita.com/ForestMountain1234/)"""  # noqa: E501,W291
+    [ForestMountain1234's Qiita](https://qiita.com/ForestMountain1234/)"""  # noqa: E501
 
-    def __init__(self, value: T_STATE | None = None) -> None:
+    def __init__(self, value: T | None = None) -> None:
         self.__value = value
-        self.__observers: list[Callable[[T_STATE | None], None]] = []
+        self.__observers: list[Callable[[T | None], None]] = []
 
-    def get(self) -> T_STATE | None:
+    def get(self) -> T | None:
         return self.__value
 
-    def set(self, new_value: T_STATE):
+    def set(self, new_value: T):
         if self.__value != new_value:
             self.__value = new_value
             self._update()
@@ -49,7 +47,7 @@ class State(Generic[T_STATE], IState):
         self.__observers.append(observer)
 
 
-class ReactiveState(Generic[T_REACTIVESTATE], IState):
+class ReactiveState(IState,Generic[T]):
 
     """
     When reliance states( State or ReactiveState ) or reliance state group is updated, functions( setted by .bind() ) is executed.
@@ -57,7 +55,7 @@ class ReactiveState(Generic[T_REACTIVESTATE], IState):
     This is based in [this article: Python(Flet)でリアクティブなUIを作る方法を考える](https://qiita.com/ForestMountain1234/items/64edacd5275c1ce4c943). And I did a little changes.
     Thanks to ForestMountain1234
     [ForestMountain1234's GitHub](https://github.com/ForestMountain1234)
-    [ForestMountain1234's Qiita](https://qiita.com/ForestMountain1234/)"""  # noqa: E501,W291
+    [ForestMountain1234's Qiita](https://qiita.com/ForestMountain1234/)"""  # noqa: E501
 
     # --original comment--
     # formula: State等を用いて最終的にT型の値を返す関数。
@@ -65,7 +63,7 @@ class ReactiveState(Generic[T_REACTIVESTATE], IState):
     # reliance_states: 依存関係にあるState, ReactiveStateをlist形式で羅列する。
 
     def __init__(
-        self, formula: Callable[[], T_REACTIVESTATE], reliance_states: list[IState]
+        self, formula: Callable[[], T], reliance_states: list[IState]
     ):
         # reliance_group is being designed and prepared
 
@@ -74,14 +72,14 @@ class ReactiveState(Generic[T_REACTIVESTATE], IState):
         # 通常のStateクラスとは違い、valueがStateである
 
         self.__formula = formula
-        self.__observers: list[Callable[[T_REACTIVESTATE | None], None]] = []
+        self.__observers: list[Callable[[T | None], None]] = []
 
         for state in reliance_states:
             # --original comment--
             # 依存関係にあるStateが変更されたら、再計算処理を実行するようにする
             state.bind(lambda: self._update())
 
-    def get(self) -> T_REACTIVESTATE | None:
+    def get(self) -> T | None:
         return self.__value.get()
 
     def _update(self):
