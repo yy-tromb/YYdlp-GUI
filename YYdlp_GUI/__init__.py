d44cc0c987f14fe28250c1ed8ba730c0ee0721e1
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2024-03-18 00:40:50 +0900

    fix import class name

--- YYdlp_GUI/__init__.py
+++ YYdlp_GUI/__init__.py
@@ -1,5 +1,5 @@
 from . import view, mycontrols, state, yt_dlp_wrapper
-from .state import State, ReactiveState, Store, StateRef, ReactiveStateRef
+from .state import State, ReactiveState, Store, StateRefs
 
 
 def main():
